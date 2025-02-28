0731b46c04d127d270b2739e26ba1ed9e5ba3a27
Austin Mordahl <amordahl@uic.edu>
2025-02-28 16:52:57 -0600

    Updated scalafmt

--- src/main/g8/src/test/scala/$package$/MainTests.scala
+++ src/main/g8/src/test/scala/$package$/MainTests.scala
@@ -4,5 +4,6 @@ import munit.{FunSuite, ScalaCheckSuite}
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends FunSuite:
-  test("not implemented".fail): ???
+  test("not implemented".fail):
+    ???
 end MySuite
